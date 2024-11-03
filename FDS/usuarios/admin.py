afbae353e50a07afdae66085de26b73a7bf7547a
Pedro Gusmao <padg@cesar.school>
2024-11-03 16:41:56 -0300

    Adicionando visualizar horários das disciplinas no repositório principal

--- FDS/usuarios/admin.py
+++ FDS/usuarios/admin.py
@@ -4,13 +4,14 @@ from django.contrib import admin
 from .models import Usuario
 from .models import Documento
 from .models import Solicitacao
-from .models import Evento, AvisoAcademico
+from .models import Evento, AvisoAcademico, Horario
 
 admin.site.register(Usuario)
 admin.site.register(Documento)
 admin.site.register(Solicitacao)
 admin.site.register(Evento)
 admin.site.register(AvisoAcademico)
+admin.site.register(Horario)
 
 @admin.register(Materia)
 class MateriaAdmin(admin.ModelAdmin):
